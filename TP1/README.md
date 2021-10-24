1b1d1e25224153e56f6b61d822ec51928965e7d2
frossi <frrossi@fi.uba.ar>
2021-10-23 23:26:15 -0300

    README update (ej5 y funciones aux

--- TP1/README.md
+++ TP1/README.md
@@ -9,9 +9,15 @@
 |    Rossi, Francisco    |  99540 |  frrossi@fi.uba.ar |
 
 ## Índice
+
+- [Trabajo Práctico 1](#trabajo-práctico-1)
+  - [Sistemas Embebidos - 2C2021 - Grupo 1](#sistemas-embebidos---2c2021---grupo-1)
+  - [Índice](#índice)
+- [Resumen](#resumen)
+- [Funciones auxiliares](#funciones-auxiliares)
+  - [TECX](#tecx)
+  - [main_region](#main_region)
 - [Ejercicio 3](#ejercicio-3)
-  * [Sub item](#subitem)
-    * [Sub Sub item](#subsubitem)
 - [Ejercicio 4](#ejercicio-4)
 - [Ejercicio 5](#ejercicio-5)
 - [Ejercicio 6](#ejercicio-6)
@@ -23,6 +29,28 @@
 
 En este repositorio se desarrollará el trabajo práctico numero 1 de la materia 86.65 - Sistemas Embebidos, FIUBA. El mismo consiste de 6 ejercicios, cuyas resoluciones podrán encontrar en este repositorio. En este informe desarrollaremos la explicación de cada uno de los ejercicios.
 
+# Funciones auxiliares
+
+En los diferentes ejercicio desarrollados, utilizaremos dos bloques de maquina de estados ya desarrollada, que nos permiten interactuar con el programa mediante la botonera de la placa `EDU-CIAA-NXP`.
+
+## TECX
+
+Este pedazo de codigo nos permite detectar si un pulsador es accionado, tiene un debouncer incorporado y nos de vuelve un valor determinador dependiendo de que pulsador es el que disparo el evento.
+
+|    TEC4   |    TEC3   |    TEC2   |    TEC1   |
+|:---------:|:---------:|:---------:|:---------:|
+| 8 [b1000] | 4 [b0100] | 2 [b0010] | 1 [b0001] |
+
+El diagrama de la maquina de estados es:
+
+![docs/images/TECX.png](docs/images/TECX.png)
+
+## main_region
+
+En la `main_region` tenemos el "handle" de los eventos dados por TECX, donde dependiendo del ejercicio que estemos resolviendo cambiara levemente, pero fundamentalmente se define que hacer para cada uno de los pulsadores, a continuacion, como ejemplo podemos ver `main_region` del [Ejercicio 5](#ejercicio-5)
+
+![docs/images/main_region.png](docs/images/main_region.png)
+
 # Ejercicio 3
 
 > Implementar (editar, simular y generar el código) el modelo de control de Panel de Control de un generador de señales (tensión de 0 a
@@ -30,7 +58,6 @@ En este repositorio se desarrollará el trabajo práctico numero 1 de la materia
 >
 > 1. Editar y simular
 > 2. Generación de código
-
 
 # Ejercicio 4
 
@@ -47,6 +74,59 @@ sentidos, control remoto de apertura/cierre, fines de carrera y señalización l
 >
 > 1. Editar y simular
 > 2. Generación de código
+
+Autor: Francisco Rossi
+
+***
+Codigo fuente: [ej5](ej5)
+
+Teniendo en cuenta el enunciado se definen los siguientes eventos:
+
+1. `evCerrarPorton`:
+    El mismo define el evento de indicar con el control remoto que se cierre la puerta.
+
+2. `evAbrirPorton`
+    Este se define al igual que el anterior, pero cuando se indicar de abrir la puerta.
+
+3. `evFinalCarrera`
+    Este evento indica que se detecto un final de carrera, puede ser tanto el de subida como el de bajada.
+
+4. `evObstaculo`
+    Est evento indica que existe un obstaculo, si bien no estaba en el enunciado, por la seguridad del mismo lo agregamos al diseno.
+
+Luego, se definen 5 estados, los nombres de los mismos son autoexplicativos:
+
+1. **Cerrado**
+2. **Abierto**
+3. **Abriendo**
+4. **Cerrando**
+5. **Parado**, en este caso vale la pena decir que se trata de un punto medio entre abierto y cerrado (en caso de obstaculo).
+
+La **maquina de estados** puede observarse en la siguiente figura:
+![docs/images/ej5_program.png](docs/images/ej5_program.png)
+
+Por simplicidad se elije que un mismo evento abre o cierre el porton (un solo boton del control remoto), dependiendo del estado en el que este justo antes de presionarlo.
+
+El programa, inicia en estado cerrado, hasta que se le de la indicacion de abrir y este entonces procedera abrirse (es decir, hasta que se detecte el final de carrera superior), en caso de que haya un obstaculo, se enviara una senal al porton para frenar y se esperar 1s para retomar la accion y asi sucesivamente hasta que no exista mas el obstaculo. Una vez que esta abierto sucede lo equivalente para cerrarse.
+
+Las siguientes funciones son las ejecutadas al ingresar en cada uno de los estados. De esta manera se inician las acciones de abrir, cerrar o de parar el porton, segun corresponda.
+
+```c
+void ej5_opAbrirPorton(Ej5* handle, const sc_integer PortNumber){
+  gpioWrite(LED1, true);
+  //accionarMotor(arriba)
+}
+void ej5_opCerrarPorton(Ej5* handle, const sc_integer PortNumber){
+  gpioWrite(LED2, true);
+  //accionarMotor(abajo)
+}
+void ej5_opPararPorton(Ej5* handle, const sc_integer PortNumber){
+  gpioWrite(LED3, true);
+  //accionarMotor(frenar)
+}
+```
+
+En este caso solo se estan llevando a cabo la senalizacion de los LEDS. Pero, en caso de tener realmente el motor, se accionaria el mismo correspondientemente (a esto refiere `accionarMotor()`).
 
 # Ejercicio 6
 
---
+++ TP1/ej5/src/main.c
@@ -0,0 +1,244 @@
+/** @brief This is a simple statechart example using Yakindu Statechart Tool
+ * Plug-in (update site: http://updates.yakindu.org/sct/mars/releases/).
+ */
+
+/** \addtogroup statechart Simple UML Statechart example.
+ ** @{ */
+
+/*==================[inclusions]=============================================*/
+
+#include "main.h"
+#include "sapi.h"       // <= sAPI header
+
+/* Include statechart header file. Be sure you run the statechart C code
+ * generation tool!
+ */
+//#include "ej5.h"
+#include "ej5.h"
+#include "TimerTicks.h"
+
+
+/*==================[macros and definitions]=================================*/
+
+#define TICKRATE_1MS	(1)				/* 1000 ticks per second */
+#define TICKRATE_MS		(TICKRATE_1MS)	/* 1000 ticks per second */
+
+
+/*==================[internal data declaration]==============================*/
+
+volatile bool SysTick_Time_Flag = false;
+
+/*! This is a state machine */
+static Ej5 statechart;
+
+/*! This is a timed state machine that requires timer services */
+#define NOF_TIMERS (sizeof(Ej5TimeEvents)/sizeof(sc_boolean))
+
+TimerTicks ticks[NOF_TIMERS];
+
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+
+/*! \file This header defines prototypes for all functions that are required
+ *  by the state machine implementation.
+ *
+ *  This is a state machine uses time events which require access to a timing
+ *  service. Thus the function prototypes:
+// *  - ej5_setTimer and
+ *  - ej5_set_timer and
+// *  - ej5_unsetTimer
+ *  - ej5_unset_timer
+ *  are defined.
+ *
+ *  This state machine makes use of operations declared in the state machines
+ *  interface or internal scopes. Thus the function prototypes:
+// *  - ej5Iface_opLED
+ *  - ej5_opLED
+ *  are defined.
+ *
+ *  These functions will be called during a 'run to completion step' (runCycle)
+ *  of the statechart.
+ *  There are some constraints that have to be considered for the
+ *  implementation of these functions:
+ *  - never call the statechart API functions from within these functions.
+ *  - make sure that the execution time is as short as possible.
+ */
+
+/** state machine user-defined external function (action)
+ *
+ * @param handle state machine instance
+ * @param LEDNumber number of LED
+ * @param onoff state machine operation parameter
+ */
+
+//void ej5Iface_opLED( Ej5* handle, sc_integer LEDNumber, sc_boolean State )
+void ej5_opLED( Ej5* handle, sc_integer LEDNumber, sc_boolean State )
+{
+	gpioWrite( (LEDR + LEDNumber), State );
+}
+
+// funciones
+
+void ej5_opAbrirPorton( Ej5* handle, const sc_integer PortNumber){
+	gpioWrite(LED1, true);
+}
+void ej5_opCerrarPorton( Ej5* handle, const sc_integer PortNumber){
+	gpioWrite(LED2, true);
+}
+void ej5_opPararPorton( Ej5* handle, const sc_integer PortNumber){
+	gpioWrite(LED3, true);
+}
+
+
+/*! * This is a timed state machine that requires timer services
+ */
+
+/*! This function has to set up timers for the time events that are required
+ *  by the state machine.
+ */
+/*! This function will be called for each time event that is relevant for a
+ *  state when a state will be entered.
+ *  \param evid An unique identifier of the event.
+ *  \time_ms The time in milli seconds
+ *  \periodic Indicates the the time event must be raised periodically until
+ *   the timer is unset
+ */
+//void ej5_setTimer( Ej5* handle, const sc_eventid evid, const sc_integer time_ms, const sc_boolean periodic )
+void ej5_set_timer( Ej5* handle, const sc_eventid evid, const sc_integer time_ms, const sc_boolean periodic )
+{
+	SetNewTimerTick(ticks, NOF_TIMERS, evid, time_ms, periodic);
+}
+
+/*! This function has to unset timers for the time events that are required
+ *  by the state machine.
+ */
+/*! This function will be called for each time event that is relevant for a
+ *  state when a state will be left.
+ *  \param evid An unique identifier of the event.
+ */
+//void ej5_unsetTimer( Ej5* handle, const sc_eventid evid )
+void ej5_unset_timer( Ej5* handle, const sc_eventid evid )
+{
+	UnsetTimerTick( ticks, NOF_TIMERS, evid );
+}
+
+
+/**
+ * @brief	Hook on Handle interrupt from SysTick timer
+ * @return	Nothing
+ */
+void myTickHook( void *ptr ){
+
+	/* The sysTick Interrupt Handler only set a Flag */
+	SysTick_Time_Flag = true;
+}
+
+
+/*! This function scan all EDU-CIAA-NXP buttons (TEC1, TEC2, TEC3 and TEC4),
+ *  and return ID of pressed button (TEC1 or TEC2 or TEC3 or TEC4)
+ *  or false if no button was pressed.
+ */
+uint32_t Buttons_GetStatus_(void) {
+	uint8_t ret = false;
+	uint32_t idx;
+
+	for (idx = 0; idx < 4; ++idx) {
+		if (gpioRead( TEC1 + idx ) == 0)
+			ret |= 1 << idx;
+	}
+	return ret;
+}
+
+
+/**
+ * @brief	main routine for statechart example: EDU-CIAA-NXP - ej5
+ * @return	Function should not exit.
+ */
+int main(void)
+{
+	uint32_t i;
+
+	uint32_t BUTTON_Status;
+
+	/* Generic Initialization */
+	boardConfig();
+
+	/* Init Ticks counter => TICKRATE_MS */
+	tickConfig( TICKRATE_MS );
+
+	/* Add Tick Hook */
+	tickCallbackSet( myTickHook, (void*)NULL );
+
+	/* Init Timer Ticks */
+	InitTimerTicks( ticks, NOF_TIMERS );
+
+	/* Statechart Initialization */
+	ej5_init( &statechart );
+	ej5_enter( &statechart );
+
+	/* LED state is toggled in the main program */
+	while (1) {
+		/* The uC sleeps waiting for an interruption */
+		__WFI();
+
+		/* When a interrupt wakes to the uC, the main program validates it,
+		 * checking the waited Flag */
+		if (SysTick_Time_Flag == true) {
+
+			/* Then reset its Flag */
+			SysTick_Time_Flag = false;
+
+			/* Then Update all Timer Ticks */
+			UpdateTimers( ticks, NOF_TIMERS );
+
+			/* Then Scan all Timer Ticks */
+			for (i = 0; i < NOF_TIMERS; i++) {
+
+				/* Then if there are pending events */
+				if (IsPendEvent( ticks, NOF_TIMERS, ticks[i].evid ) == true) {
+
+					/* Then Raise an Event -> Ticks.evid => OK */
+//					ej5_raiseTimeEvent( &statechart, ticks[i].evid );
+					ej5_raise_time_event( &statechart, ticks[i].evid );
+
+					/* Then Mark as Attached -> Ticks.evid => OK */
+					MarkAsAttEvent( ticks, NOF_TIMERS, ticks[i].evid );
+				}
+			}
+
+			/* Then Get status of buttons */
+			BUTTON_Status = Buttons_GetStatus_();
+
+			/* Then if there are a pressed button */
+			if (BUTTON_Status != 0)
+
+				/* Then Raise an Event -> evTECXOprimodo => OK,
+				 * and Value of pressed button -> viTecla */
+//				ej5Iface_raise_evTECXOprimido(&statechart, BUTTON_Status);
+				ej5_raise_evTECXOprimido(&statechart, BUTTON_Status);
+
+
+
+			else
+				/* Then else Raise an Event -> evTECXNoOprimido => OK */
+//				ej5Iface_raise_evTECXNoOprimido(&statechart);
+				ej5_raise_evTECXNoOprimido(&statechart);
+
+			/* Then Run an Cycle of Statechart */
+//			ej5_runCycle(&statechart);		// Run Cycle of Statechart
+		}
+	}
+}
+
+
+/** @} doxygen end group definition */
+
+/*==================[end of file]============================================*/
