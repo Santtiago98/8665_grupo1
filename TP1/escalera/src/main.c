6bda013455a42d4117067d206809febad1ed09aa
ipipe <pipernoignacio@gmail.com>
2021-10-25 16:41:15 -0300

    Agregado de comentarios de operaciones

--- TP1/escalera/src/main.c
+++ TP1/escalera/src/main.c
@@ -1,43 +1,3 @@
-/* Copyright 2017, Pablo Ridolfi, Juan Esteban Alarcon, Juan Manuel Cruz
- * All rights reserved.
- *
- * This file is part of Workspace.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- */
- 
-/** @brief This is a simple statechart example using Yakindu Statechart Tool
- * Plug-in (update site: http://updates.yakindu.org/sct/mars/releases/).
- */
-
-/** \addtogroup statechart Simple UML Statechart example.
- ** @{ */
-
 /*==================[inclusions]=============================================*/
 
 #include "main.h"
@@ -69,39 +29,44 @@ static EscaleraMecanica statechart;
 TimerTicks ticks[NOF_TIMERS];
 
 
-/*==================[internal functions declaration]=========================*/
-
-/*==================[internal data definition]===============================*/
-
-/*==================[external data definition]===============================*/
-
-/*==================[internal functions definition]==========================*/
-
 /*==================[external functions definition]==========================*/
 
+/*! Esta funcion para o prende la escalera (en este caso apaga y prende el LEDR y el LEDG).
+ *  @param handle instancia de máquina de estados
+ *  @param state estado anterior de la escalera
+ */
 void escaleraMecanicaIface_opSetLight(const EscaleraMecanica* handle, const sc_boolean state)
 {
 
 	if(state == false){
 		gpioWrite( (LEDG), false);
 		gpioWrite( (LEDR), true );
+		// apaga el motor
 	}
 	else{
 		gpioWrite( (LEDR), false);
 		gpioWrite( (LEDG), true);
+		// enciende el motor
+
 	}
 
 }
 
+/*! Esta funcion cambia la velocidad del motor.
+ *  @param handle instancia de máquina de estados
+ *  @param speed  opción de velocidad
+ */
 void escaleraMecanicaIface_opSetSpeed(const EscaleraMecanica* handle, const sc_integer speed)
 {
 	if(speed == SPEED_1){
-		gpioWrite( (LEDR+3), false);
-		gpioWrite( (LEDR+4), true);
-	}
-	if(speed == SPEED_2){
 		gpioWrite( (LEDR+3), true);
 		gpioWrite( (LEDR+4), false);
+		// motor a velocidad 1
+	}
+	if(speed == SPEED_2){
+		gpioWrite( (LEDR+3), false);
+		gpioWrite( (LEDR+4), true);
+		// motor a velocidad 2
 	}
 
 }
@@ -138,6 +103,8 @@ void escaleraMecanicaIface_opCount(const EscaleraMecanica* handle, const sc_inte
  * @param LEDNumber number of LED
  * @param onoff state machine operation parameter
  */
+
+
 void escaleraMecanicaIface_opLED( EscaleraMecanica* handle, sc_integer LEDNumber, sc_boolean State )
 {
 	gpioWrite( (LEDR + LEDNumber), State );
